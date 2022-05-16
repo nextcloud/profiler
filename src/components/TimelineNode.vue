bc0d413c5e657a3ef8474806ec8588cfcdd25311
Robin Appelman <robin@icewind.nl>
2022-05-16 13:24:21 +0200

    allow zooming in the profiler timeline
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- src/components/TimelineNode.vue
+++ src/components/TimelineNode.vue
@@ -6,15 +6,15 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <template>
 	<div class="node-group">
-		<div class="node" :title="`${event.id} - ${event.description} ${duration}ms`">
+		<div class="node" :title="`${event.id} - ${event.description} ${duration}ms`" @click="$emit('click', event)">
 			{{ event.id }} - {{ event.description }} {{ duration }}ms
 		</div>
-		<div v-if="children.length > 0" class="children">
+		<div v-if="children.length > 0" class="children" :style="`height: ${childHeight}px`">
 			<div v-for="(event) in children"
 				:key="event.id"
 				:style="event.style"
 				class="child">
-				<TimelineNode :event="event" />
+				<TimelineNode :event="event" @click="(childEvent) => $emit('click', childEvent)" />
 			</div>
 		</div>
 	</div>
@@ -29,14 +29,18 @@ export default {
 			required: true,
 		},
 	},
+	emits: ['click'],
 	computed: {
 		children() {
 			return this.event.children.map(child => {
 				const startRelative = (child.start - this.event.start) / this.event.duration
 				const durationRelative = child.duration / this.event.duration
-				child.style = `left: ${startRelative * 100}%; width: ${durationRelative * 100}%`
+				child.style = `left: ${(startRelative) * 100}%; width: ${durationRelative * 100}%`
 				return child
 			})
+		},
+		childHeight() {
+			return 24 * this.event.childDepth
 		},
 		duration() {
 			return (this.event.duration * 1000).toFixed(1)
@@ -67,13 +71,14 @@ div.node {
 	background-color: #00000044;
 	padding: 0 2px;
 	white-space: nowrap;
+	cursor: pointer;
 }
 
 div.child {
-	position: relative;
+	position: absolute;
 	display: inline-block;
 	vertical-align: top;
-	top: 0;
+	//top: 0;
 	overflow-x: hidden;
 	margin: 0;
 }
